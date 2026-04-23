e1932a7219abc33c42d27b2bbc8a1c94c7093ef3
eithar25 <131480817+eithar25@users.noreply.github.com>
2026-04-23 13:38:26 +0200

    Update book_ticket.sql

--- book_ticket.sql
+++ book_ticket.sql
@@ -1,14 +1,10 @@
 USE [CinemaDB]
 GO
-
 /****** Object:  StoredProcedure [dbo].[BOOK_TICKET]    Script Date: 4/23/2026 8:19:47 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-
 CREATE PROCEDURE [dbo].[BOOK_TICKET]
 @userId INT,
 @showId INT,
@@ -16,10 +12,8 @@ CREATE PROCEDURE [dbo].[BOOK_TICKET]
 @paymentMethod VARCHAR(50)
 AS
 BEGIN
-
     BEGIN TRY
         BEGIN TRANSACTION;
-
         DECLARE @hallId INT;
         DECLARE @totalPrice MONEY;
         DECLARE @bookingId INT;
@@ -41,7 +35,6 @@ BEGIN
             ROLLBACK;
             RETURN;
         END
-
         SELECT @hallId = hallId FROM Show WHERE showId = @showId;
          IF EXISTS (
             SELECT 1
@@ -65,17 +58,16 @@ BEGIN
                 WHERE i.status = 'Reserved'
                 AND i.showId = @showId
                 AND i.hallId = @hallId
-            
+
         )
         BEGIN
             PRINT 'Seat already booked';
             ROLLBACK;
             RETURN;
         END
-        
+
         INSERT INTO Booking (bookingDate, status, userId, showId)
         VALUES (GETDATE(), 'Pending', @userId, @showId);
-
         SET @bookingId = SCOPE_IDENTITY();
         INSERT INTO Has (seatNumber, hallId, bookingId)
         SELECT DISTINCT seatNumber, @hallId, @bookingId
@@ -88,28 +80,22 @@ BEGIN
         WHERE i.showId = @showId
           AND i.hallId = @hallId
           AND i.status = 'Available';
-        SELECT @totalPrice = ISNULL(SUM(I.price), 0)
+        SELECT @totalPrice = ISNULL(SUM(S.price), 0)
         FROM Has H
-        JOIN Includes I
-             ON H.seatNumber = I.seatNumber
-             AND H.hallId  = I.hallId
-             AND I.showId  = @showId
+        JOIN Seat S 
+            ON H.seatNumber = S.seatNumber 
+           AND H.hallId = S.hallId
         WHERE H.bookingId = @bookingId;
         INSERT INTO Payment (bookingId, paymentMethod, date, amount, status)
         VALUES (@bookingId, @paymentMethod, GETDATE(), @totalPrice, 'Success');
         UPDATE Booking
         SET status = 'Confirmed'
         WHERE bookingId = @bookingId;
-
         COMMIT;
-
     END TRY
-
     BEGIN CATCH
         ROLLBACK;
         PRINT ERROR_MESSAGE();
     END CATCH
-
 END
 GO
-
