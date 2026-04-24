d9bdbf4aadf0fd1c792fcf64d5acd1989ceae12e
Mariam Arafa <mariamprofessional16@gmail.com>
2026-04-24 14:48:37 +0300

    Update cancel_booking.sql

--- cancel_booking.sql
+++ cancel_booking.sql
@@ -1,74 +1,70 @@
-USE [CinemaDB]
-GO
-
-/****** Object:  StoredProcedure [dbo].[CancelBooking]    Script Date: 4/23/2026 8:21:21 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE PROCEDURE [dbo].[CancelBooking]
-@bookingId INT,
-@userId INT,
-@refundAmount MONEY OUTPUT
+    @bookingId INT,
+    @userId INT,
+    @refundAmount DECIMAL(10,2) OUTPUT
 AS 
 BEGIN
-DECLARE @showId INT
-DECLARE @TotalPrice  MONEY 
-DECLARE @ShowDateTime  DATETIME
-SELECT @TotalPrice = ISNULL(SUM(S.price), 0)
-FROM Has H
-JOIN Seat S ON H.seatNumber = S.seatNumber AND H.hallId = S.hallId
-WHERE H.bookingId = @bookingId;
-SELECT
-@showId=B.showId,
-@ShowDateTime = DATEADD(MINUTE, DATEDIFF(MINUTE, 0, CAST(S.startTime AS DATETIME)), CAST(S.date AS DATETIME))
-FROM Booking B INNER JOIN Show S
-ON S.showId=B.showId
-WHERE B.bookingId=@bookingId
-AND
-B.userId=@userId
-IF @showId IS NULL
+    SET NOCOUNT ON;
+    DECLARE @showId INT;
+    DECLARE @TotalPrice DECIMAL(10,2);
+    DECLARE @ShowDateTime DATETIME;
+
+    SET @TotalPrice = dbo.fn_CalculateTotalPrice(@bookingId, 0);
+
+    SELECT
+        @showId = B.ShowID,
+        @ShowDateTime = CAST(S.Date AS DATETIME) + CAST(S.start_time AS DATETIME)
+    FROM Booking B 
+    INNER JOIN Show S ON S.ShowID = B.ShowID
+    WHERE B.BookingID = @bookingId AND B.UserID = @userId;
+
+    IF @showId IS NULL
     BEGIN
         RAISERROR('Booking not found or does not belong to this user.', 16, 1);
         RETURN;
     END
-IF EXISTS (
-    SELECT 1 FROM Booking WHERE bookingId=@bookingId AND status='Cancelled'
-    )
+
+    IF EXISTS (SELECT 1 FROM Booking WHERE BookingID = @bookingId AND Status = 'Cancelled')
     BEGIN
         RAISERROR('Booking is already cancelled.', 16, 1);
         RETURN;
     END
-IF @ShowDateTime< DATEADD(HOUR, 24, GETDATE())
+
+    IF @ShowDateTime < DATEADD(HOUR, 24, GETDATE())
     BEGIN
         RAISERROR('Cannot cancel a booking within 24 hours of showtime.', 16, 1);
         RETURN;
     END
-BEGIN TRY
-    BEGIN TRANSACTION;
-    UPDATE I
-    SET I.status='Available'
-    FROM Includes I INNER JOIN HAS H
-    ON I.seatNumber=H.seatNumber
-    AND I.hallId=H.hallId
-       WHERE I.showId=@showId
-       AND   H.bookingId = @bookingId
-    UPDATE Booking
-    SET status= 'Cancelled'
-    WHERE bookingId=@bookingId
-    INSERT INTO Payment (bookingId, paymentMethod, date, amount, status)
-    VALUES (@bookingId, 'Refund', GETDATE(), @totalPrice, 'Processed');
-    SET @refundAmount=@TotalPrice
-    COMMIT TRANSACTION;
-        PRINT 'Booking cancelled. Refund of ' + CAST(@totalPrice AS VARCHAR) + ' will be processed.'
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        UPDATE I
+        SET I.Status = 'Available'
+        FROM Includes I 
+        INNER JOIN Has H ON I.SeatNumber = H.SeatNumber AND I.HallID = H.HallID
+        WHERE I.ShowID = @showId AND H.BookingID = @bookingId;
+
+        UPDATE Booking
+        SET Status = 'Cancelled'
+        WHERE BookingID = @bookingId;
+
+        UPDATE [User]
+        SET balance = balance + @TotalPrice
+        WHERE UserID = @userId;
+
+        INSERT INTO Payment (BookingID, Status, Date)
+        VALUES (@bookingId, 'Refunded', GETDATE());
+
+        SET @refundAmount = @TotalPrice;
+
+        COMMIT TRANSACTION;
+        PRINT 'Booking cancelled successfully. Amount ' + CAST(@TotalPrice AS VARCHAR) + ' returned to your balance.';
+        
     END TRY
     BEGIN CATCH
-        ROLLBACK TRANSACTION;
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
         THROW;
     END CATCH
-
-END
+END;
 GO
-
