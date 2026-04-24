561de5f2b83823c3dc87c198c649dee82ef1583b
Mariam Arafa <mariamprofessional16@gmail.com>
2026-04-24 15:35:44 +0300

    Update showAllAvaliableSeatsForASpecificShow.sql

--- showAllAvaliableSeatsForASpecificShow.sql
+++ showAllAvaliableSeatsForASpecificShow.sql
@@ -1,28 +1,16 @@
-USE [CinemaDB]
-GO
-
-/****** Object:  StoredProcedure [dbo].[showAllAvaliableSeatsForASpecificShow]    Script Date: 4/23/2026 8:24:52 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE PROCEDURE [dbo].[showAllAvaliableSeatsForASpecificShow](@showid INT )
+CREATE PROCEDURE [dbo].[showAllAvaliableSeatsForASpecificShow]
+    @showid INT 
 AS
 BEGIN
-SELECT     
-    Seat.seatNumber,
-    Seat.rowNumber,
-    Seat.type,
-    Seat.hallId,
-    Includes.showId
-FROM Seat INNER JOIN Includes 
-ON Seat.seatNumber = Includes.seatNumber
-AND Seat.hallId = Includes.hallId
-WHERE
-Includes.showId=showid
-AND Includes.status='available'
+    SELECT      
+        S.SeatNumber,
+        S.Raw_number,  
+        S.Type,
+        S.HallID,
+        I.ShowID
+    FROM Seat S
+    INNER JOIN Includes I ON S.SeatNumber = I.SeatNumber AND S.HallID = I.HallID
+    WHERE I.ShowID = @showid
+      AND dbo.checkSeatAvailability(S.SeatNumber, S.HallID, I.ShowID) = 'Available';
 END
 GO
-
