f65f921083084af508262ea63a880ac741dbbcd4
Mariam Arafa <mariamprofessional16@gmail.com>
2026-04-24 15:25:20 +0300

    Refactor DeleteUser procedure to handle seat updates

--- delete_user.sql
+++ delete_user.sql
@@ -1,37 +1,42 @@
-USE [CinemaDB]
-GO
-
-/****** Object:  StoredProcedure [dbo].[DeleteUser]    Script Date: 4/23/2026 8:23:50 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE PROCEDURE [dbo].[DeleteUser]
-@userId INT
+    @userId INT
 AS
 BEGIN
-IF NOT EXISTS(
-SELECT 1 FROM [User]  where userId=@userId
-)
-BEGIN 
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT 1 FROM [User] WHERE UserID = @userId)
+    BEGIN 
         RAISERROR('INVALID USERID.', 16, 1);
         RETURN;
-END
-BEGIN TRANSACTION;
-BEGIN TRY
-DELETE FROM Has WHERE bookingId IN (SELECT bookingId FROM Booking WHERE userId=@userId )
-DELETE FROM Booking WHERE userId=@userId
-DELETE FROM User_Phone WHERE userId=@userId
-DELETE FROM [User] WHERE userId=@userId
-    COMMIT TRANSACTION;
-PRINT 'User deleted successfully.';
-END TRY
-BEGIN CATCH
-    ROLLBACK TRANSACTION;
-    THROW;
-END CATCH
+    END
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        UPDATE I
+        SET I.Status = 'Available'
+        FROM Includes I
+        INNER JOIN Has H ON I.SeatNumber = H.SeatNumber AND I.HallID = H.HallID
+        INNER JOIN Booking B ON H.BookingID = B.BookingID
+        WHERE B.UserID = @userId AND B.Status <> 'Cancelled';
+
+        DELETE FROM Payment 
+        WHERE BookingID IN (SELECT BookingID FROM Booking WHERE UserID = @userId);
+
+        DELETE FROM Has 
+        WHERE BookingID IN (SELECT BookingID FROM Booking WHERE UserID = @userId);
+
+        DELETE FROM Booking WHERE UserID = @userId;
+
+        DELETE FROM user_phone WHERE UserID = @userId;
+
+        DELETE FROM [User] WHERE UserID = @userId;
+
+        COMMIT TRANSACTION;
+        PRINT 'User and all related records (including freed seats) deleted successfully.';
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
 END
 GO
-
